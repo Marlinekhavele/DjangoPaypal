c33a08748a1575d4fbb5f72314f257911f70568f
MarlineKhavele <marlinekhavele6@gmail.com>
2020-04-06 19:44:23 +0300

    made some changes

--- djangopaypal_client/settings.py
+++ djangopaypal_client/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "!wi6t^84yng-xnmsgtnh*%yi-0ut**+w$26tily#6lp*-@_mhn"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ "127.0.0.1"]
 
