c53a6e3cb038f83deae5ee0a0401722d767c215e
MarlineKhavele <marlinekhavele6@gmail.com>
2020-04-06 14:44:21 +0300

    Added checkout page

--- base/urls.py
+++ base/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 
 	
 
-    path('', views.store, name="store"),
-    path('checkout/<int:pk>/', views.checkout, name="checkout"),
-    path('complete/', views.paymentComplete, name="complete"),
+    # path('', views.store, name="store"),
+    # path('checkout/<int:pk>/', views.checkout, name="checkout"),
+    # path('complete/', views.paymentComplete, name="complete"),
 ]
