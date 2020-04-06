10d623e36791fc208d86895574d1c839780861dd
MarlineKhavele <marlinekhavele6@gmail.com>
2020-04-06 17:36:35 +0300

    Modified settings

--- djangopaypal_client/settings.py
+++ djangopaypal_client/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -51,8 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "djangopaySTATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-pal_client.urls"
+ROOT_URLCONF = "djangopaypal_client.urls"
 
 TEMPLATES = [
     {
@@ -119,10 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
