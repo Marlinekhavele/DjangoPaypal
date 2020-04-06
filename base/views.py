3b92c3f4b42238457042b36ec128f5d1fd3f7df3
MarlineKhavele <marlinekhavele6@gmail.com>
2020-04-06 14:38:49 +0300

    Added files

--- base/views.py
+++ base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+from .models import Product, Order
 
 # Create your views here.
+def simpleCheckout(request):
+    return render(request, "base/simple_checkout.html")
