e7b406a234496a8454c3e0830a7d2edcb07251ea
MarlineKhavele <marlinekhavele6@gmail.com>
2020-04-06 16:01:11 +0300

    modified checkout

--- base/views.py
+++ base/views.py
@@ -6,3 +6,20 @@ from .models import Product, Order
 # Create your views here.
 def simpleCheckout(request):
     return render(request, "base/simple_checkout.html")
+
+
+def store(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "base/store.html", context)
+
+
+def checkout(request, pk):
+    product = Product.objects.get(id=pk)
+    context = {"product": product}
+    return render(request, "base/checkout.html", context)
+
+
+def paymentComplete(request):
+    body = json.loads(request.body)
+    return JsonResponse("Payment completed!", safe=False)
